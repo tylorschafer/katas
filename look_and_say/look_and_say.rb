e52fde24ea218ced4bd87121ebb8ede9a026a0c2
Mark Josef <mcphage@gmail.com>
2011-09-27 16:06:08 -0400

    Solutions for uke and look and say problems

--- look_and_say/look_and_say.rb
+++ look_and_say/look_and_say.rb
@@ -1,15 +1,16 @@
 class LookAndSay
   
   def initialize(string)
-    # Implement me!
+    raise ArgumentError, "Can only accept a sequence of digits" unless string.to_s =~ /^[0-9]+$/
+    @split = string.to_s.scan( /((.)\2*)/ ).map { |match| match.first }
   end
 
   def next
-    # Implement me!
+    LookAndSay.new @split.map { |sequence| [ sequence.length, sequence[0,1] ] }
   end
 
   def to_s
-    # Implement me!
+    @split.join
   end
   
 end
