5528dc641a4f11b0ba3cf011dc03b4a0f9a24409
Mark Josef <mcphage@gmail.com>
2011-10-10 10:22:07 -0400

    Fiddling with LaS and Uke, solution for THE

--- look_and_say/look_and_say.rb
+++ look_and_say/look_and_say.rb
@@ -2,15 +2,15 @@ class LookAndSay
   
   def initialize(string)
     raise ArgumentError, "Can only accept a sequence of digits" unless string.to_s =~ /^[0-9]+$/
-    @split = string.to_s.scan( /((.)\2*)/ ).map { |match| match.first }
+    @split = string.to_s.scan( /((.)\2*)/ ).map { |chars, char| [ chars.length, char ] }
   end
 
   def next
-    LookAndSay.new @split.map { |sequence| [ sequence.length, sequence[0,1] ] }
+    LookAndSay.new @split
   end
 
   def to_s
-    @split.join
+    @split.map { |length, char| char * length }.join
   end
   
 end
--- texas_hold_em/texas_hold_em.rb
+++ texas_hold_em/texas_hold_em.rb
@@ -1,11 +1,67 @@
+require 'matchers'
+
 class TexasHoldEm
-  
+
   def initialize(cards)
-    # Implement me!
+    split_cards = cards.split
+    
+    raise ArgumentError, "Must pass in 7 cards" unless split_cards.length == 7
+    raise ArgumentError, "Must pass in distinct cards" unless split_cards.length == split_cards.uniq.length
+    
+    @cards = split_cards.map { |str| Card.new(str) }
+    @hands = @cards.combination(5).map { |cards| Hand.new(cards) }
   end
   
   def best_hand
-    # Implement me!
+    hand_matcher = HandMatcher.new
+    best_hand = @hands.sort_by { |hand| hand_matcher.evaluate(hand) }.last
+    
+    best_hand.match.to_s
   end
   
+end
+
+class Card
+  
+  attr_accessor :rank, :suit
+  
+  RANKS    = %w(2 3 4 5 6 7 8 9 10 J Q K A)
+  MAX_RANK = RANKS.length - 1
+  
+  def initialize(string)
+    match = string.match /(.+)([HSCD])/
+    
+    raise ArgumentError, "Invalid card: #{ string }" unless match && RANKS.include?(match[1])
+    
+    @rank = RANKS.index match[1]
+    @suit = match[2]
+  end
+  
+  def to_s
+    RANKS[@rank] + @suit
+  end
+  
+end
+
+class Hand
+  
+  attr_accessor :cards, :match
+
+  def initialize(cards)
+    @cards = cards
+  end
+  
+end
+
+class PokerMatch
+
+  def initialize(name, high_rank)
+    @name = name
+    @high = high_rank
+  end
+  
+  def to_s
+    "#{ @name } (#{ Card::RANKS[ @high ] } high)"
+  end
+
 end
